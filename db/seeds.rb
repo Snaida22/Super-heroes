5d564cadb14dc5ec122d8c5d9d0a38bfd89b1dce
Snaida.Kabalika <snaida.kabalika@student.moringaschool.com>
2022-10-10 16:59:36 +0300

    add seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Seeding data'
+
+#Creation of Heroes seed data
+hero1 = Hero.create(name: "Kamala Khan", super_name: "Ms. Marvel")
+hero2 = Hero.create(name: "Doreen Green", super_name: "Squirrel Girl")
+hero3 = Hero.create(name: "Gwen Stacy", super_name: "Spider-Gwen")
+
+#Creation of Powers seed data
+diving = Power.create(name: "super strength",description: "gives the wielder super-human strengths")
+flight = Power.create(name: "flight",description: "gives the wielder the ability to fly through the skies at supersonic speed")
+
+
+#Creation of HeroPowers seed data 
+HeroPower.create("strength": "weak", "hero_id":hero1.id, "power_id":diving.id)
+HeroPower.create("strength": "average", "hero_id":hero2.id, "power_id":flight.id)
+HeroPower.create("strength": "strong", "hero_id":hero3.id, "power_id":diving.id)
+
+puts 'Done!'
